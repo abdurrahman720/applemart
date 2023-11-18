56c3dcc4146356328d6f83030900422f3a632ac9
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-11-18 19:37:34 +0600

    product image

--- app/components/product/ProductDetails.tsx
+++ app/components/product/ProductDetails.tsx
@@ -5,6 +5,7 @@ import { useCallback, useState } from "react";
 import SetColor from "./SetColor";
 import SetQuantity from "./SetQuantity";
 import Button from "../Button";
+import ProductImage from "./ProductImage";
 
 interface ProductDetailsProps{
     product: any;
@@ -85,7 +86,7 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-            <div>Images</div>
+         <ProductImage cartProduct={cartProduct} product={product} handleColorSelect={handleColorSelect} />
             <div className="flex flex-col gap-1 text-slate-500 text-sm ">
                 <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
                 <div className="flex items-center gap-2">
@@ -117,7 +118,7 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
                 <div className="max-w-[300px]">
                 <Button label="Add to cart" onClick = {()=>{}}  />
              </div>
-            </div>
+            P</div>
         </div>
     );
 };
